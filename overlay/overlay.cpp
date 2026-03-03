fefcc66e301b6babfdb7e09f4c4276b9e65f5e48
Maurice Lambert <50479118+mauricelambert@users.noreply.github.com>
2026-03-03 08:16:02 +0100

    Update overlay.cpp

--- overlay/overlay.cpp
+++ overlay/overlay.cpp
@@ -1,3 +1,20 @@
+//    This package implements fun, creative, and sometimes
+//    slightly chaotic GUI experiments. 
+//    Copyright (C) 2026  Maurice Lambert
+
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 // "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvars64.bat"
 // cl /LD overlay.cpp /O2 /MT
 
@@ -161,4 +178,5 @@ BOOL APIENTRY DllMain(HMODULE hModule,
         GdiplusShutdown(g_gdiplusToken);
     }
     return TRUE;
+
 }
