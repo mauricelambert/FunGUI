d60b699109b3e7a0226586d3567a53f99ac6192f
Maurice Lambert <50479118+mauricelambert@users.noreply.github.com>
2026-03-03 08:14:00 +0100

    Update pillow_image_overlay_win32.py

--- overlay/pillow_image_overlay_win32.py
+++ overlay/pillow_image_overlay_win32.py
@@ -1,3 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+###################
+#    This package implements fun, creative, and sometimes
+#    slightly chaotic GUI experiments. 
+#    Copyright (C) 2026  Maurice Lambert
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+###################
+
+"""
+This package implements fun, creative, and sometimes
+slightly chaotic GUI experiments.
+"""
+
+__version__ = "0.0.1"
+__author__ = "Maurice Lambert"
+__author_email__ = "mauricelambert434@gmail.com"
+__maintainer__ = "Maurice Lambert"
+__maintainer_email__ = "mauricelambert434@gmail.com"
+__description__ = """
+This package implements fun, creative, and sometimes
+slightly chaotic GUI experiments.
+"""
+license = "GPL-3.0 License"
+__url__ = "https://github.com/mauricelambert/FunGUI"
+
+copyright = """
+FunGUI  Copyright (C) 2026  Maurice Lambert
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it
+under certain conditions.
+"""
+__license__ = license
+__copyright__ = copyright
+
+__all__ = []
+print(copyright)
+
 from PIL import Image
 import ctypes
 from ctypes import wintypes
@@ -184,4 +235,5 @@ def main():
         time.sleep(0.016)
 
 if __name__ == "__main__":
+
     main()
